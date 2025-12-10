df9922737b086fed4cf5c7b575e523e9ee339189
Quân <24127228@student.hcmus.edu.vn>
2025-12-10 15:10:08 +0700

    Create easy_medium_advanced_problems.sql

---
+++ scripts/easy_medium_advanced_problems.sql
@@ -0,0 +1,35 @@
+--Easy Problems
+
+--Coffee Consumers Count
+	How many people in each city are eastimated to consume coffee, given that 25% of the population does ?
+
+-- Total Revenue from Coffee Sales 
+	What is the total revenue generated from coffee sales across all cities in last qtr of 2023?
+
+-- Sales Count for Each Product
+	How many units of each coffee product have been sold?
+
+-- Average Sales Amount per City
+	What is the average sales amount per customer in each city?
+
+-- City Population and Coffee Consumers
+	Provide a list of cities along with their populations and estimated coffee consumers
+
+--Medium Problems 
+
+-- Top Selling Products by City
+	What are the top 3 selling products in each city based on sales volume?
+
+-- Customer Segmentation by City
+	How many unique customers are there in each city who have purchased coffee products?
+
+-- Impact of estimated Rent on Sales
+	Find each city and their average sal per custome and average rent per customer 
+
+-- Advanced Problems 
+
+-- Monthly Sales Growth
+	Sales growth rate: Calculate the percentage growth (or decline) in sales over different time periods (monthly)
+
+-- Market Potential Analysis
+	Identify top 3 city based on highest sales, return city name, total sale, total rent, total customers, and estimated coffee consumer
