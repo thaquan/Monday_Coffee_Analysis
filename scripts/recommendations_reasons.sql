3bb669181beb75130535ae021602069c1445e0bc
Quân <24127228@student.hcmus.edu.vn>
2025-12-10 15:14:32 +0700

    Create recommendations_reasons.sql

---
+++ scripts/recommendations_reasons.sql
@@ -0,0 +1,16 @@
+-- Recommendations & Reasons 
+
+-- City 1: Pune
+	Average rent per customer is very low 
+	Highest total revenue
+	Average sales per customer is also high
+
+-- City 2: Delhi
+	Highest estimated coffee consumers at 7.7 million
+	Highest total number of customers, which is 68
+	Average rent per customer is 330 (still under 500)
+
+-- City 3: Jaipur
+	Highest number of customers, which is 69
+	Average rent per customer is very low, at 156
+	Average sales per customer is better at 11.6k
